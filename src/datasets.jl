68a5339f1b95ffedefffd66dd4725c9d984bc5cd
Matthieu Lesnoff <matthieu.lesnoff@gmail.com>
2023-03-11 21:40:16 +0100

    Update datasets.jl

--- src/datasets.jl
+++ src/datasets.jl
@@ -9,7 +9,17 @@ datasets()
 """
 function datasets()
     path = joinpath(@__DIR__, "..", "data")
-    readdir(path)
+    z = readdir(path)
+    n = length(z)
+    m = mod(n, 4)
+    if m > 0
+        u = repeat([""], 4 - m)
+        v = [z; u]
+    else
+        v = copy(z)
+    end
+    k = length(v)
+    reshape(v, Int64(k / 4), 4)
 end
 
 
